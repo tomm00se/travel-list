6beeb7dddf2b8c43c84894488ed70002ceb2400c
tomm00se <tom@moose17.dev>
2026-01-18 14:19:28 +0000

    Refactor App component and enhance styling for packing list functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Toothbrush", quantity: 1, packed: true },
+];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = (newItem) => {
+    setItems((items) => [...items, newItem]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+export const Logo = () => {
+  return <h1>Travel List</h1>;
+};
+
+export const Form = ({ onAddItems }) => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    onAddItems(newItem);
+    setDescription("");
+    setQuantity(1);
+  };
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need to pack?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((dropdownItem) => {
+          return (
+            <option value={dropdownItem} key={dropdownItem}>
+              {dropdownItem}
+            </option>
+          );
+        })}
+      </select>
+      <input
+        type="text"
+        placeholder="Holiday items..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      ></input>
+      <button>Add to List!</button>
+    </form>
+  );
+};
+
+export const PackingList = ({ items }) => {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => {
+          return <Item item={item} key={item.id} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const Item = ({ item }) => {
+  return (
+    <li>
+      <span
+        style={item.packed ? { textDecoration: "line-through" } : {}}
+      >{`${item.quantity}x ${item.description}`}</span>
+      <button>❌</button>
+    </li>
+  );
+};
+
+export const Stats = () => {
+  return (
+    <footer className="stats">
+      Stats: You have x items on your list and have already packed x (x%)
+    </footer>
+  );
+};
